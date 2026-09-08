9ec91afe3cad60966b68957d0e385243654acfca
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-08 12:24:38 -0400

    Initial commit

---
+++ ${lowercaseAppName}/tests/PaymentCardTest.py
@@ -0,0 +1,42 @@
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from bankingsystem.models.PaymentCard import PaymentCard
+from bankingsystem.delegates.PaymentCardDelegate import PaymentCardDelegate
+
+ #======================================================================
+# 
+# Encapsulates data for model PaymentCard
+#
+# @author Harbormaster Dev Team
+#
+#======================================================================
+
+#======================================================================
+# Class PaymentCardTest Declaration
+#======================================================================
+class PaymentCardTest (TestCase) :
+	def test_crud(self) :
+		paymentCard = PaymentCard()
+		paymentCard.cardNumber = "default cardNumber field value"
+		paymentCard.embossedName = "default embossedName field value"
+		paymentCard.expiryMonth = 22
+		paymentCard.expiryYear = 22
+		paymentCard.cardType = "default cardType field value"
+		paymentCard.cardStatus = "default cardStatus field value"
+		paymentCard.network = "default network field value"
+		
+		delegate = PaymentCardDelegate()
+		responseObj = delegate.create(paymentCard)
+		
+		self.assertEqual(responseObj, delegate.get( responseObj.id ))
+	
+		allObj = delegate.getAll()
+		self.assertEqual(allObj.count(), 1 )		
+		delegate.delete(responseObj.id)
+		
+		allObj = delegate.getAll()
+		self.assertEqual(allObj.count(), 0 )		
+
+
---
+++ ${lowercaseAppName}/tests/ScreeningResultTest.py
@@ -0,0 +1,38 @@
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from bankingsystem.models.ScreeningResult import ScreeningResult
+from bankingsystem.delegates.ScreeningResultDelegate import ScreeningResultDelegate
+
+ #======================================================================
+# 
+# Encapsulates data for model ScreeningResult
+#
+# @author Harbormaster Dev Team
+#
+#======================================================================
+
+#======================================================================
+# Class ScreeningResultTest Declaration
+#======================================================================
+class ScreeningResultTest (TestCase) :
+	def test_crud(self) :
+		screeningResult = ScreeningResult()
+		screeningResult.screeningDate = datetime.datetime.now()
+		screeningResult.provider = "default provider field value"
+		screeningResult.outcome = "default outcome field value"
+		
+		delegate = ScreeningResultDelegate()
+		responseObj = delegate.create(screeningResult)
+		
+		self.assertEqual(responseObj, delegate.get( responseObj.id ))
+	
+		allObj = delegate.getAll()
+		self.assertEqual(allObj.count(), 1 )		
+		delegate.delete(responseObj.id)
+		
+		allObj = delegate.getAll()
+		self.assertEqual(allObj.count(), 0 )		
+
+
